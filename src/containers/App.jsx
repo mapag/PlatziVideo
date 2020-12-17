31be48ea6772d6ae1207dff55c2a62255b78ee40
Mateo Pagniez <62345597+mapag@users.noreply.github.com>
2020-12-17 11:45:03 -0300

    fixed indent, import react router

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -8,40 +8,34 @@ import Footer from '../components/Footer';
 import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
-const API = 'http://localhost:3000/initalState'
+const API = 'http://localhost:3000/initalState';
 
 const App = () => {
   const initialState = useInitialState(API);
   return initialState.length === 0 ? <h1>Loading...</h1> : (
-    <div className="App">
+    <div className='App'>
       <Header />
       <Search />
-      {initialState.mylist.length > 0 &&
-        <Categories title="Mi Lista">
+      {initialState.mylist.length > 0 && (
+        <Categories title='Mi Lista'>
           <Carousel>
-            {initialState.mylist.map(item =>
-              <CarouselItem key={item.id} {...item} />
-            )}
+            {initialState.mylist.map(item => <CarouselItem key={item.id} {...item} />)}
           </Carousel>
         </Categories>
-      }
-      <Categories title="Tendencias">
+      )}
+      <Categories title='Tendencias'>
         <Carousel>
-          {initialState.trends.map(item =>
-            <CarouselItem key={item.id} {...item} />
-          )}
+          {initialState.trends.map(item => <CarouselItem key={item.id} {...item} />)}
         </Carousel>
       </Categories>
-      <Categories title="Originales de Platzi Video">
+      <Categories title='Originales de Platzi Video'>
         <Carousel>
-          {initialState.originals.map(item =>
-            <CarouselItem key={item.id} {...item} />
-          )}
+          {initialState.originals.map(item => <CarouselItem key={item.id} {...item} />)}
         </Carousel>
       </Categories>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
